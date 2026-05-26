4427b333732525af7dfc1c5fea22bbc4f1c4f294
caiquefgoncalves <caiquefachinigoncalves@gmail.com>
2026-05-26 11:17:32 -0300

    Add files via upload

---
+++ src/pages/RelatorioDoador.jsx
@@ -0,0 +1,9 @@
+import RelatorioDoador1 from "../components/RelatorioDoador1/RelatorioDoador1.jsx";
+
+export default function RelatorioDoador({ api }) {
+    return (
+        <>
+            <RelatorioDoador1 api={api} />
+        </>
+    )
+}
---
+++ src/pages/RelatorioOng.jsx
@@ -0,0 +1,10 @@
+// src/pages/RelatorioOng.jsx
+import RelatorioOng1 from "../components/RelatorioOng1/RelatorioOng1.jsx";
+
+export default function RelatorioOng({ api }) {
+    return (
+        <>
+            <RelatorioOng1 api={api} />
+        </>
+    )
+}
