3df57f089910772e05ead0f961c5f9fa625b98fd
caiquefgoncalves <caiquefachinigoncalves@gmail.com>
2026-06-01 09:34:56 -0300

    Add files via upload

---
+++ src/pages/CriarStory.jsx
@@ -0,0 +1,9 @@
+import CriarStory1 from "../components/CriarStory1/CriarStory1.jsx";
+
+export default function CriarStory({ api }) {
+    return (
+        <>
+            <CriarStory1 api = { api }/>
+        </>
+    )
+}
